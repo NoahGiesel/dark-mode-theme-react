bd0d4d1d644b442607a848b262c74440c642490a
noahgiesel <noahgiesel@gmail.com>
2021-10-17 22:13:00 +0200

    formatter for vscode

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import useDarkTheme from "./useDarkTheme"
 function App() {
   const [dark, toggleDarkMode] = useDarkTheme();
 
-  return (
+  return ( 
     <div className="App">
       <h1>Dark theme is set to {dark ? "Dark" : "Light"}</h1>
       <button onClick={() => toggleDarkMode() }>Change dark theme</button>
--- src/useDarkTheme.js
+++ src/useDarkTheme.js
@@ -1,26 +1,26 @@
-import {useState,useEffect} from "react"
+import {
+    useState,
+    useEffect
+} from "react"
 
 export default function useDarkTheme() {
 
 
-    const [dark, setDark] = useState(localStorage.getItem("theme") === "dark" ? true :false);
+    const [dark, setDark] = useState(localStorage.getItem("theme") === "dark" ? true : false);
 
-    useEffect( () => {
-        if(dark){
+    useEffect(() => {
+        if (dark) {
             document.body.classList.add("dark-mode");
             localStorage.setItem("theme", "dark")
-        }else { 
+        } else {
             document.body.classList.remove("dark-mode")
             localStorage.setItem("theme", "light")
         }
-    },[dark]);
+    }, [dark]);
 
-    const toggleDarkMode = ()=> {
+    const toggleDarkMode = () => {
         setDark(!dark)
     }
 
     return [dark, toggleDarkMode]
 }
-
-
- 
