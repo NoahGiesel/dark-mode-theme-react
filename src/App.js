5c27309b34a3ff537ae8ddc360d6381ef25df00b
Noah Giesel <44894110+NoahGiesel@users.noreply.github.com>
2023-04-01 16:09:45 +0200

    comment for custom hook

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import useDarkTheme from "./useDarkTheme"
 
 function App() {
+  // dark is the currently returned value of the custom hook
+  // toggleDarkMode is the fn that you call to trigger the change of theme inside the custom hook
   const [dark, toggleDarkMode] = useDarkTheme();
 
   return ( 
